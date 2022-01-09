a7b3fd09c31e9ccbf8b825d1667ec7f96e69b687
Ruben Gutierrez <rubdeivis@gmail.com>
2022-01-09 16:31:44 -0400

    wait room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -5,7 +5,26 @@ const Room = () => {
 
   console.log("user logged", user);
 
-  return <div></div>;
+  return (
+    <div className="grid place-content-center mt-24">
+      <div className="flex flex-col items-center justify-center bg-gray-100">
+        <div
+          className="         flex flex-col
+          bg-white
+          shadow-md
+          px-4
+          sm:px-6
+          md:px-8
+          lg:px-10
+          py-8
+          rounded-3xl
+          w-50
+          max-w-md
+        "
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Room;
--- src/pages/WaitRoom.tsx
+++ src/pages/WaitRoom.tsx
@@ -1,7 +1,98 @@
+const mockParticipants = [
+  { id: 1, username: "juan" },
+  { id: 2, username: "juan" },
+  { id: 3, username: "juan" },
+];
+
 const WaitRoom = () => {
+  const handleInitGame = () => {
+    // socket login
+    console.log("init game");
+  };
+
+  const handleLogout = () => {
+    // socket login
+    console.log("logout");
+  };
+
   return (
-    <div>
-      <h3>WaitRoom</h3>
+    <div className="grid place-content-center mt-24">
+      <div className="flex flex-col items-center justify-center bg-gray-100">
+        <div
+          className="         flex flex-col
+          bg-white
+          shadow-md
+          px-4
+          sm:px-6
+          md:px-8
+          lg:px-10
+          py-8
+          rounded-3xl
+          w-50
+          max-w-md
+        "
+        >
+          <div className="font-medium self-center text-xl sm:text-3xl text-gray-800">
+            Sala de espera
+          </div>
+          <p className="my-5">creado: usuario creador</p>
+
+          <hr />
+          <p>participantes: </p>
+          <ul>
+            {mockParticipants.map((part) => (
+              <li className="my-3" key={part.id}>
+                {part.username}
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex flex-col">
+            <button
+              onClick={handleInitGame}
+              className="                  flex
+                  mt-2
+                  items-center
+                  justify-center
+                  disabled:opacity-50
+                  focus:outline-none
+                  text-white text-sm
+                  sm:text-base
+                  bg-blue-500
+                  hover:bg-blue-600
+                  rounded-2xl
+                  py-2
+                  w-full
+                  transition
+                  duration-150
+                  ease-in"
+            >
+              Iniciar
+            </button>
+            <button
+              onClick={handleLogout}
+              className="                  flex
+                  mt-2
+                  items-center
+                  justify-center
+                  disabled:opacity-50
+                  focus:outline-none
+                  text-white text-sm
+                  sm:text-base
+                  bg-blue-500
+                  hover:bg-blue-600
+                  rounded-2xl
+                  py-2
+                  w-full
+                  transition
+                  duration-150
+                  ease-in"
+            >
+              Salir
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
